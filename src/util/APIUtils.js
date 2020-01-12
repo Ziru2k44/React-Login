bac449e8d065f42e96f38f6d1a181fe2be746597
Ziru2k44 <bernardo.b.lourenco@gmail.com>
2020-01-12 18:10:10 +0000

    first commit

--- src/util/APIUtils.js
+++ src/util/APIUtils.js
@@ -29,7 +29,7 @@ export function getCurrentUser() {
     }
 
     return request({
-        url: API_BASE_URL + "/user/me",
+        url: API_BASE_URL + "/user",
         method: 'GET'
     });
 }
@@ -44,7 +44,7 @@ export function login(loginRequest) {
 
 export function getPhotos() {
     return request({
-        url: API_BASE_URL + "/user/me/photos",
+        url: API_BASE_URL + "/facebook/albums",
         method: 'GET'
     });
 }
